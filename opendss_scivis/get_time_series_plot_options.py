d9f3507c8295bea84dd368def57bed1ec8c3765b
Peter Rochford <peter.rochford@xatorcorp.com>
2023-08-21 18:12:50 -0400

    Modularized into a set of functions.

--- opendss_scivis/get_time_series_plot_options.py
+++ opendss_scivis/get_time_series_plot_options.py
@@ -1,27 +1,25 @@
-def get_time_series_plot_options(data,**kwargs):
+def _default_options() -> dict:
     '''
-    Get optional arguments for time_series_plot function.
+    Set default optional arguments for time_series_plot function.
     
-    Retrieves the optional arguments supplied to the time_series_plot 
-    function as a variable-length keyword argument list (*KWARGS), and
-    returns the values in an OPTION dictionary. Default values are 
-    assigned to selected optional arguments. The function will terminate
-    with an error if an unrecognized optional argument is supplied.
-            
-    ToDo: update as package evolves
+    Sets the default optional arguments for the TIME_SERIES_PLOT 
+    function in an OPTION dictionary. Default values are 
+    assigned to selected optional arguments. 
     
     INPUTS:
-    data    : time series of quantities in a dictionary
-    *kwargs : variable-length keyword argument list. The keywords by 
-              definition are dictionaries with keys that must correspond to 
-              one choices given in OUTPUTS below.
-    
+    None
+        
     OUTPUTS:
     option : dictionary containing option values. (Refer to 
-             display_target_diagram_options function for more information.)
+             display_display_time_series_options function for more information.)
     option['alpha']           : blending of symbol face color (0.0 
                                 transparent through 1.0 opaque). (Default : 1.0)
-    option['axislim']         : axes limits [xmin, xmax, ymin, ymax] (Default: data values)
+    option['axislim']         : axes limits for each subplot provided as a dictionary,
+                                [xmin, xmax, ymin, ymax] 
+                                (Default: data values), e.g. for a subplot of the
+                                variable Frequency: option['axislim]['Frequency'].
+                                Note that values of None can be used in the list to specify
+                                the min/max ranges are determined from the data. 
     option['linespec']  : line specification (default solid black, 'k-')
     option['linewidth'] : line width specification (default rcParams 'lines.linewidth')
     option['circles']         : radii of circles to draw to indicate 
@@ -32,6 +30,10 @@ def get_time_series_plot_options(data,**kwargs):
                                  markers to CMapZData values ('on') or min to
                                  max range of CMapZData values ('off').
                                  (Default : 'on')
+    option['layout']           : matrix layout for subplots specified as a list 
+                                 [m,n] where m is number of rows and n number of columns.
+                                 A value of m = 0 lets package specify number of rows
+                                 per figure. (Default: [0,2])
     option['locationcolorbar'] : location for the colorbar, 'NorthOutside' or
                                  'EastOutside'
     option['markerdisplayed'] : markers to use for individual experiments
@@ -59,31 +61,21 @@ def get_time_series_plot_options(data,**kwargs):
                                 (empty by default)
     option['yticklabelpos']   : position of the tick labels along the y-axis 
                                 (empty by default)
-  
-    Author: Peter A. Rochford
-            Xator Corporation
-            www.xatorcorp.com
-
-    Created on Apr 27, 2022
-    Revised on Apr  4, 2023
     '''
     from matplotlib import rcParams
-
-    nargin = len(kwargs)
-    number_variables = len(data.keys())
 
     # Set default parameters for all options
     option = {}
     option['alpha'] = 1.0
     option['axeslabel'] = []
-    option['axislim'] = [None] * number_variables
-    option['axismax']=[]
-    option['axismin']=[]
+    option['axislim'] = None
+    option['layout'] = [0, 2]
     option['linespec'] = 'k-'
     option['linewidth'] = rcParams.get('lines.linewidth')
 
-    option['index_x'] = None
-    option['index_y'] = None
+    option['index_x'] = None #used internally
+    option['index_y'] = None #used internally
+    option['subplot'] = None #used internally
 
     option['markercolor'] = 'r'
     option['markerdisplayed'] = 'line'
@@ -99,10 +91,33 @@ def get_time_series_plot_options(data,**kwargs):
     option['titlecolorbar'] = ''
     option['xticklabelpos'] = []
     option['yticklabelpos'] = []
-    if nargin == 0:
-        # No options requested, so return with only defaults
-        return option
+         
+    return option
+
+def _get_options(option, **kwargs) -> dict:
+    '''
+    Get values for optional arguments for time_series_plot function.
     
+    Gets the default optional arguments for the TIME_SERIES_PLOT 
+    function in an OPTION dictionary. 
+    
+    INPUTS:
+    option  : dictionary containing default option values
+    *kwargs : variable-length keyword argument list. The keywords by 
+              definition are dictionaries with keys that must correspond to 
+              one of the choices given in the _default_options function.
+        
+    OUTPUTS:
+    option : dictionary containing option values
+  
+    Author:
+    
+    Peter A. Rochford
+        rochford.peter1@gmail.com
+
+    Created on Aug 8, 2023
+    Revised on Aug 8, 2023
+    '''    
     # Check for valid keys and values in dictionary
     for optname, optvalue in kwargs.items():
         optname = optname.lower()
@@ -127,5 +142,59 @@ def get_time_series_plot_options(data,**kwargs):
                 else:
                     raise ValueError('markerlabel value is not a list or dictionary: ' +
                                      str(optvalue))
+ 
+        del optname, optvalue   
+    
+    # Set layout if an overlay
+    if option['overlay'] == 'on':
+        option['layout'][0] = number_rows
+        option['layout'][1] = number_cols
+    else: 
+        number_rows = option['layout'][0]
+        number_cols = option['layout'][1] 
+
+    return option   
+
+def get_time_series_plot_options(**kwargs):
+    '''
+    Get optional arguments for time_series_plot function.
+    
+    Retrieves the optional arguments supplied to the time_series_plot 
+    function as a variable-length keyword argument list (*KWARGS), and
+    returns the values in an OPTION dictionary. Default values are 
+    assigned to selected optional arguments. The function will terminate
+    with an error if an unrecognized optional argument is supplied.
+            
+    ToDo: update as package evolves
+    
+    INPUTS:
+    *kwargs : variable-length keyword argument list. The keywords by 
+              definition are dictionaries with keys that must correspond to 
+              one choices given in OUTPUTS below.
+    
+    OUTPUTS:
+    option : dictionary containing option values. (Refer to 
+             display_display_time_series_options function for more information.)
+  
+    Author: Peter A. Rochford
+            Xator Corporation
+            www.xatorcorp.com
+
+    Created on Aug 8, 2023
+    Revised on Aug 8, 2023
+    '''
+    global number_cols, number_rows, subplot_axis
+
+    nargin = len(kwargs)
+
+    # Set default parameters for all options
+    option = _default_options()
+
+    # No options requested, so return with only defaults
+    if nargin == 0: return option
+
+    # Check for valid keys and values in dictionary
+    # Allows user to override options specified in CSV file
+    option = _get_options(option, **kwargs)
     
     return option
