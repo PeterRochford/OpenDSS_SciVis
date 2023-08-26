a80c3b0021799a8b2270392ca5b4aab658dd20cb
Peter Rochford <peter.rochford@xatorcorp.com>
2023-08-26 16:53:41 -0400

    Corrected naming of get_files_in_directory.

--- opendss_scivis/__init__.py
+++ opendss_scivis/__init__.py
@@ -2,7 +2,7 @@ from .add_line_legend import add_line_legend
 from .add_marker_legend import add_marker_legend
 from .colname_stats import colname_stats
 from .file_stats import file_stats
-from .get_file_directory import get_files_in_directory
+from .get_files_in_directory import get_files_in_directory
 from .get_time_series import get_time_series
 from .get_time_series_axes import get_time_series_axes
 from .get_time_series_plot_options import get_time_series_plot_options
